aeea316db7a6416e82ae2cf567729ee71b3932f3
ViToN <vitnow2009@gmail.com>
2022-12-15 11:03:45 +0500

    add server

--- main.go
+++ main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Handler struct {
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+
+	case http.MethodGet:
+
+		name := r.FormValue("name")
+
+		fmt.Fprintf(w, "Parsed query-param with key \"name\": %s", name)
+	case http.MethodPost:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+
+			return
+
+		}
+
+		defer r.Body.Close()
+		fmt.Fprintf(w, "Parsed request body: %s\n", string(body))
+	}
+}
 
 func main() {
+	handler := &Handler{}
+	http.Handle("/", handler)
 
-	fmt.Println("Course project start")
+	srv := &http.Server{
+
+		Addr: ":1881",
+
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	srv.ListenAndServe()
 }
